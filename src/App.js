4ddb3a8bcfb1ac81e88aaa8e6b5948df4e39f87c
Adrian Hagjija <adrianhagjija@hotmail.com>
2021-02-10 17:15:27 +0100

    new features

--- src/App.js
+++ src/App.js
@@ -3,17 +3,16 @@ import Navigation from "./Navigation";
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import Accueil from "./Accueil";
 import About from "./About";
-import Contactpage from "./Contactpage";
+import ContactPage from "./Contactpage";
 
 function App() {
   return (
   <div>
    <Router>
        <Navigation />
-
        <Route path={"/"} exact component={Accueil}/>
        <Route path={"/about"} exact component={About}/>
-       <Route path={"/contact"} exact component={Contactpage}/>
+       <Route path={"/contact"} exact component={ContactPage}/>
    </Router>
 </div>
 );
--- src/Contactpage.js
+++ src/Contactpage.js
@@ -1,31 +1,62 @@
 import './App.css';
-import {Form, FormLabel, FormControl, FormGroup, Button} from "react-bootstrap";
+import {Form, Button, Col} from "react-bootstrap";
+import React from "react";
 
-function Contactpage() {
+
+class ContactPage extends React.Component{
+
+    state = {
+        prenom: null,
+        date: null
+    };
+
+    onChangePrenom (val) {
+        //this.setState({prenom: val.target.value})
+        //this.setState(state.prenom = val.target.value
+    }
+
+    onChangeDate (val) {
+        this.setState({date: val.target.value})
+    }
+
+    handleSubmit = async (event) => {
+
+        console.log(this.state.prenom)
+
+        const axios = require('axios')
+        const json = {
+            prenom: event.target[0].value,
+            date: event.target[1].value
+        }
+        console.log(json);
+
+        const result = await axios.post('http://localhost:8000/marco', {json})
+        console.log(result)
+        event.preventDefault();
+    }
+render() {
     return(
         <div>
-            <Form method="POST" action="/post-feedback">
-                <Form.Group controlId="exampleForm.ControlInput1">
-                    <Form.Label>Full name</Form.Label>
-                    <Form.Control type="text" placeholder="name" required />
-                </Form.Group>
-                <Form.Group controlId="exampleForm.ControlInput1">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="name@example.com" required />
-                </Form.Group>
-                <Form.Group controlId="exampleForm.ControlTextarea1">
-                    <Form.Label>Comment</Form.Label>
-                    <Form.Control as="textarea" rows={3} required />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
+            <Form>
+                <Form.Row>
+                    <Form.Group as={Col} controlId="mid">
+                        <Form.Label>Member ID</Form.Label>
+                        <Form.Control required type="id" value={this.state.prenom} onChange={this.onChangePrenom} />
+                    </Form.Group>
+                    <Form.Group  as={Col} controlId="joiningDate">
+                        <Form.Label>Joining Date</Form.Label>
+                        <Form.Control required type="date" value={this.state.date} onChange={this.onChangeDate}/>
+                    </Form.Group>
+                </Form.Row>
+                <Button variant="primary" onClick={this.handleSubmit}>
+                    Create
                 </Button>
             </Form>
             <a href={"/view-feedbacks"}>View feedbacks</a>
         </div>
     );
+};
 }
 
-
-export default Contactpage;
+export default ContactPage;
 
